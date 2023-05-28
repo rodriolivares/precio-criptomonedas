1356ae0cdda86a34068e0586623d49145e40276e
rodriolivares <riolivares.dev@gmail.com>
2023-05-28 19:49:44 -0300

    first commit

---
+++ src/components/Resultado.jsx
@@ -0,0 +1,51 @@
+import styled from "@emotion/styled"
+
+const Contenedor = styled.div`
+    color: #FFF;
+    font-family: 'Lato', sans-serif;
+
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    margin-top: 30px;
+`
+
+const Imagen = styled.img`
+    display: block;
+    width: 120px;
+`
+const Texto = styled.p`
+    font-size: 18px;
+    span {
+        font-weight: 700;
+    }
+`
+const Precio = styled.p`
+    font-size: 24px;
+    span {
+        font-weight: 700;
+    }
+`
+
+
+const Resultado = ({resultado}) => {
+    const {PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE} = resultado
+
+    return (
+        <Contenedor>
+            <Imagen 
+                src={`https://cryptocompare.com/${IMAGEURL}`} 
+                alt="Imagen Cripto" 
+            />
+            <div>
+                <Precio>El Precio es de: <span>{PRICE}</span></Precio>
+                <Texto>El Precio más alto del día es de: <span>{HIGHDAY}</span></Texto>
+                <Texto>El Precio más bajo del día es de: <span>{LOWDAY}</span></Texto>
+                <Texto>La Variacion en las ultimas 24 horas es de: <span>{CHANGEPCT24HOUR}</span></Texto>            
+                <Texto>La ultima actualizacion fue el: <span>{PRICE}</span></Texto>
+            </div>
+        </Contenedor>
+    )
+}
+
+export default Resultado
